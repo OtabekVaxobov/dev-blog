916144d11d5e49c748dd13fdb1a0cd84b72a050d
OtabekVaxobov <elot2017@mail.ru>
2023-08-25 18:10:06 +0500

    feat: add all

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Providers } from "./providers/providers";
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'dev-blog',
+  description: 'all kind of developers blog',
+  authors: { name: 'OtabekVaxobov', url: 'https://github.com/OtabekVaxobov' },
+  applicationName: 'Dev-blog',
+  manifest: 'https://dev-blog.uz/manifest.json',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          {children}
+        </Providers></body>
+    </html>
+  )
+}
