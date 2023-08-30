cda6669d4cf5afca35cc9ab979cd9d2c22fb8a7f
OtabekVaxobov <elot2017@mail.ru>
2023-08-30 18:57:52 +0500

    fix: footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 export default function Footer() {
   const dt = new Date();
   return <>
-    <footer className="bg-white dark:bg-gray-900 sticky top-[100vh]">
+    <footer className="sticky top-[100vh]">
       <div className="container px-6 py-8 mx-auto">
         <div className="flex flex-col items-center text-center">
 
 
           <div className="flex flex-wrap justify-center mt-6 -mx-4">
-            <Link href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Home </Link>
+            <Link href="#" className="mx-4 text-sm transition-colors duration-300 hover:text-blue-500  dark:hover:text-blue-400" aria-label="Reddit"> Home </Link>
 
-            <Link href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> About </Link>
+            <Link href="#" className="mx-4 text-sm transition-colors duration-300 hover:text-blue-500  dark:hover:text-blue-400" aria-label="Reddit"> About </Link>
 
-            <Link href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Teams </Link>
+            <Link href="#" className="mx-4 text-sm transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Teams </Link>
 
-            <Link href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Privacy </Link>
+            <Link href="#" className="mx-4 text-sm transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Privacy </Link>
 
-            <Link href="#" className="mx-4 text-sm text-gray-600 transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Cookies </Link>
+            <Link href="#" className="mx-4 text-sm transition-colors duration-300 hover:text-blue-500 dark:text-gray-300 dark:hover:text-blue-400" aria-label="Reddit"> Cookies </Link>
           </div>
 
         </div>
@@ -26,10 +26,10 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 md:my-10 dark:border-gray-700" />
 
         <div className="flex flex-col items-center sm:flex-row sm:justify-between">
-          <p className="text-sm text-gray-500 dark:text-gray-300">© Copyright {dt.getFullYear()}. All Rights Reserved.</p>
+          <p className="text-sm dark:text-gray-300">© Copyright {dt.getFullYear()}. All Rights Reserved.</p>
 
           <div className="flex -mx-2">
-            <Link href="#" className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Reddit">
+            <Link href="#" className="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Reddit">
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -38,7 +38,7 @@ export default function Footer() {
               </svg>
             </Link>
 
-            <Link href="#" className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Facebook">
+            <Link href="#" className="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Facebook">
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -47,7 +47,7 @@ export default function Footer() {
               </svg>
             </Link>
 
-            <Link href="#" className="mx-2 text-gray-600 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Github">
+            <Link href="#" className="mx-2 transition-colors duration-300 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400" aria-label="Github">
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24" fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <path
