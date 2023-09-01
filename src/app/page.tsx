bdd2bd7a8b6ec781e8b1aa88b1e3de873c9f4697
Otabek_Vaxobov <elot2017@mail.ru>
2023-09-01 22:47:40 +0500

    fix: hide metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 
 import { Suspense } from 'react';
 import Loading from '../components/loading';
 
-export const metadata: Metadata = {
-  title: 'dev-blog',
-  description: 'all kind of developers blog',
-  authors: { name: 'OtabekVaxobov', url: 'https://github.com/OtabekVaxobov' },
-  applicationName: 'Dev-blog',
-  // manifest: 'https://dev-blog.uz/manifest.json',
-};
+// export const metadata: Metadata = {
+//   title: 'dev-blog',
+//   description: 'all kind of developers blog',
+//   authors: { name: 'OtabekVaxobov', url: 'https://github.com/OtabekVaxobov' },
+//   applicationName: 'Dev-blog',
+//   // manifest: 'https://dev-blog.uz/manifest.json',
+// };
 
 export default function Home() {
   return (
